802143da72e65d83f38795f6ed7ab929ee5d4f80
Grigorii Heifetz <g.heifetz@arenadata.io>
2026-07-13 13:06:11 +0300

    Initial commit
    Add required lines to Makefile to start building ggcheckmigrate, also add basic
    empty utility structure and functions definitions.

---
+++ checkmigrate/checkmigrate.go
@@ -0,0 +1,20 @@
+package ggcheckmigrate
+
+import (
+	"github.com/spf13/cobra"
+)
+
+func DoInit(cmd *cobra.Command) {
+
+}
+
+func DoSetup() {
+
+}
+
+func DoCheckMigrate() {
+
+}
+
+func DoTeardown() {
+}
