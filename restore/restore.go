bae0364077e284786cda5ea434e66d8f825d4243
Robert Gorchakov <r.gorchakov@arenadata.io>
2026-07-30 09:36:01 +0000

    Lil space

--- restore/restore.go
+++ restore/restore.go
@@ -415,7 +415,7 @@ func editStatementsRedirectSchema(statements []toc.StatementWithType, redirectSc
 	// This expression matches a GRANT or REVOKE statement on any object and captures the old schema name
 	permissionsRE := regexp.MustCompile(fmt.Sprintf(`(?m)(^(?:REVOKE|GRANT) .+ ON .+?) (%s)((\..+)? (?:FROM|TO) .+)`, schemaMatch))
 	// This expression matches an ALTER PARTITION ... SET SUBPARTITION TEMPLATE statement and captures old schema name
-	alterPartitionRE := regexp.MustCompile(fmt.Sprintf(`(ALTER TABLE(?: ONLY)?) (%s)(\..+)( ALTER PARTITION (?:.+))*(\nSET SUBPARTITION TEMPLATE)`, schemaMatch))
+	alterPartitionRE := regexp.MustCompile(fmt.Sprintf(`(ALTER TABLE(?: ONLY)?) (%s)(\..+) (ALTER PARTITION (?:.+) )*(\nSET SUBPARTITION TEMPLATE)`, schemaMatch))
 	// This expression matches an ATTACH PARTITION statement and captures both the parent and child schema names
 	attachRE := regexp.MustCompile(fmt.Sprintf(`(ALTER TABLE(?: ONLY)?) (%[1]s)(\..+ ATTACH PARTITION) (%[1]s)(\..+)`, schemaMatch))
 	// This expression matches a '<schema>.<table>'::regclass::oid expression.
@@ -446,7 +446,7 @@ func editStatementsRedirectSchema(statements []toc.StatementWithType, redirectSc
 		// which will have schema qualified tables.
 		// Also don't set replaced, as otherwise we won't change CREATE TABLE schema.
 		if statements[i].ObjectType == toc.OBJ_TABLE && strings.Contains(statement, "SET SUBPARTITION TEMPLATE") {
-			statement = alterPartitionRE.ReplaceAllString(statement, fmt.Sprintf("$1 %[1]s$3 $4$5", redirectSchema))
+			statement = alterPartitionRE.ReplaceAllString(statement, fmt.Sprintf("$1 %[1]s$3$4$5", redirectSchema))
 		}
 
 		// ALTER TABLE schema.root ATTACH PARTITION schema.leaf needs two schema replacements
