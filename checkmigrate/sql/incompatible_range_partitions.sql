e14b4671d5b6ba0667974764a9ad5392d974480a
Vladimir Sarmin <v.sarmin@arenadata.io>
2026-08-04 23:45:49 +0300

    Filter incompatible range partition checks

--- checkmigrate/sql/incompatible_range_partitions.sql
+++ checkmigrate/sql/incompatible_range_partitions.sql
@@ -11,6 +11,7 @@ JOIN pg_catalog.pg_attribute a ON a.attrelid = c.oid AND a.attnum = ANY(p.paratt
 JOIN pg_catalog.pg_type t ON t.oid = a.atttypid
 JOIN pg_catalog.pg_class child_relation ON child_relation.oid = partition_rule.parchildrelid
 JOIN pg_catalog.pg_namespace child_namespace ON child_namespace.oid = child_relation.relnamespace
-WHERE t.typname IN ('text', 'float8', 'float4', 'numeric')
+WHERE p.parkind = 'r'
+  AND t.typname IN ('text', 'float8', 'float4', 'numeric')
   AND (NOT partition_rule.parrangestartincl OR partition_rule.parrangeendincl)
 ORDER BY n.nspname, c.relname, t.typname, child_namespace.nspname, child_relation.relname;
